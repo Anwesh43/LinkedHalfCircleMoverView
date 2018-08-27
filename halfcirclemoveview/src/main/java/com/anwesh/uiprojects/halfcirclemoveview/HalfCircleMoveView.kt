1238c6d590e81a70282899c01d258e646e24f55e
Anwesh43 <anweshthecool0@gmail.com>
2018-08-27 11:33:18 +0530

    adding static create method

--- halfcirclemoveview/src/main/java/com/anwesh/uiprojects/halfcirclemoveview/HalfCircleMoveView.kt
+++ halfcirclemoveview/src/main/java/com/anwesh/uiprojects/halfcirclemoveview/HalfCircleMoveView.kt
@@ -4,6 +4,7 @@ package com.anwesh.uiprojects.halfcirclemoveview
  * Created by anweshmishra on 27/08/18.
  */
 
+import android.app.Activity
 import android.view.View
 import android.view.MotionEvent
 import android.content.Context
@@ -185,6 +186,14 @@ class HalfCircleMoveView (ctx : Context) : View(ctx) {
             hcm.startUpdating {
                 animator.start()
             }
+        }
+    }
+
+    companion object {
+        fun create(activity : Activity) : HalfCircleMoveView {
+            val view : HalfCircleMoveView = HalfCircleMoveView(activity)
+            activity.setContentView(view)
+            return view
         }
     }
 }
