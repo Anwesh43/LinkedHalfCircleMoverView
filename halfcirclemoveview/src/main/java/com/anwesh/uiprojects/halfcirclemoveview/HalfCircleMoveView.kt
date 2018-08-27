db0198485fc7b6c47c4bf24720befe96c79f8577
Anwesh43 <anweshthecool0@gmail.com>
2018-08-27 11:14:32 +0530

    adding drawHCNode canvas extension method

--- halfcirclemoveview/src/main/java/com/anwesh/uiprojects/halfcirclemoveview/HalfCircleMoveView.kt
+++ halfcirclemoveview/src/main/java/com/anwesh/uiprojects/halfcirclemoveview/HalfCircleMoveView.kt
@@ -8,10 +8,30 @@ import android.view.View
 import android.view.MotionEvent
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.RectF
 
 val nodes : Int = 5
+
+fun Canvas.drawHCNode(i : Int, scale : Float, paint : Paint) {
+    val sc1 : Float = Math.min(0.5f, scale) * 2
+    val sc2 : Float = Math.min(0.5f, Math.max(0f, scale - 0.5f)) * 2
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val r : Float = gap / 4
+    val factor : Float =  1f - 2 * (i % 2)
+    paint.style = Paint.Style.STROKE
+    paint.strokeWidth = Math.min(w, h) / 50
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.color = Color.parseColor("#1976D2")
+    save()
+    translate(w / 2, gap / 3 + gap / 2 + gap * i * sc1)
+    rotate(180f * factor * sc2)
+    drawArc(RectF(-r, -r, r, r), 90f * (1 + factor), 180f, false, paint)
+    restore()
+}
 
 class HalfCircleMoveView (ctx : Context) : View(ctx) {
 
@@ -33,7 +53,7 @@ class HalfCircleMoveView (ctx : Context) : View(ctx) {
     data class State(var scale : Float = 0f, var prevScale : Float = 0f, var dir : Float = 0f) {
 
         fun update(cb : (Float) -> Int) {
-            scale += 0.1f * dir
+            scale += 0.05f * dir
             if (Math.abs(scale - prevScale) > 1) {
                 scale = prevScale + dir
                 dir = 0f
